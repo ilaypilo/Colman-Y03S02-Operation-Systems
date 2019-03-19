d6546c6908206b4ae7abcff43d134a7426a457fb
student <ilay.pilo@gmail.com>
2019-03-19 20:32:17 +0200

    added comments

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,3 +1,13 @@
+/*
+Operation System - Igor Rochlin
+19/03/19
+Ex1 - Compare 2 files
+Bar Zrihan 203285770 בר זריהן
+Ilay Pilosof 304961519 עילי פילוסוף
+
+*/
+
+
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -41,21 +51,24 @@ int main(int argc, char* argv[])
 		readBytes2 = read(fdin2, buffer2, BUFFER_SIZE);
 		if (readBytes1 != readBytes2)
 		{
+			// DONT PRINT
 			//printf("files are different 1\n");
 			return 1;
 		}
 
+		// compate the buffers
 		// if we got here -> readBytes1 = readBytes2 
-		// so we can use one of them as size
+		// so we can use one of them as the size of the buffers
 		if (0 != memcmp(buffer1, buffer2, readBytes1))
 		{
+			// DONT PRINT
 			//printf("files are different 2\n");
 			return 1;
 		}
 
 	} while ((readBytes1 == BUFFER_SIZE) && (readBytes2 == BUFFER_SIZE));
 
-	// files are identical!
+	// if we got here -> files are identical!
 	return 2;
 }
 
