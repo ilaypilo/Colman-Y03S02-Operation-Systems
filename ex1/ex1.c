366d2113e93bcd9ffc0c0f7e10e2331454162a0d
student <ilay.pilo@gmail.com>
2019-03-27 19:05:10 +0200

    on error return 0

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -27,23 +27,23 @@ int main(int argc, char* argv[])
 
 
 	if (argc != 3) {
-		printf("Usage: %s <1st file> <2nd file>\n", argv[0]);
-		return -1;
+		//printf("Usage: %s <1st file> <2nd file>\n", argv[0]);
+		return 0;
 	}
 
 	fdin1 = open(argv[1], O_RDONLY);
 	if (fdin1 < 0) /* means file open did not take place */
 	{
-		printf("error open 1st %s\n", argv[1]);
-		return -1;
+		//printf("error open 1st %s\n", argv[1]);
+		return 0;
 	}
 
 	fdin2 = open(argv[2], O_RDONLY);
 	if (fdin2 < 0) /* means file open did not take place */
 	{
-		printf("error open 2nd %s\n", argv[2]);
+		//printf("error open 2nd %s\n", argv[2]);
 		close(fdin1);
-		return -1;
+		return 0;
 	}
 
 	do
@@ -53,10 +53,10 @@ int main(int argc, char* argv[])
                 // check read error
 		if (readBytes1 < 0 || readBytes2 < 0)
 		{
-			printf("error while reading the files\n");
+			//printf("error while reading the files\n");
 			close(fdin1);
 			close(fdin2);
-			return -1;
+			return 0;
 		}
                 // check if bytes read are equal
 		if (readBytes1 != readBytes2)
