b8d20fe1f2ef0929051f9b7f45051f63f493dc9b
Bar Zrihan <bzrihan@drivenets.com>
2019-05-04 16:08:44 +0300

    Implement FCFSfindTurnAroundTime method calculation

---
+++ ex3/main.c
@@ -0,0 +1,132 @@
+/*
+Operation Systems - Igor Rochlin
+27/04/19
+Ex3 - Scheduler
+Bar Zrihan 203285770 בר זריהן
+Ilay Pilosof 304961519 עילי פילוסוף
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+int readline(int fd, char *buffer) {
+	int res;
+	int count = 0;
+	char *ptrLine = buffer;
+	while ((res = read(fd, ptrLine, 1)) > 0) {
+		// break if EOL (end of line)
+		if (*ptrLine == '\n' || *ptrLine == '\0') {
+			// null terminate the file
+			*ptrLine = '\0';
+			break;
+		}
+		++ptrLine;
+		++count;
+	}
+	if (res < 0) /* means file read failed */
+	{
+		// printf("error reading the line\n");
+		_exit(res);
+	}
+	// printf("done reading the line: %s\n", buffer);
+	return count;
+}
+
+
+void FCFSfindTurnAroundTime(int processes[], int n, int bt[]) {
+	int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+	// waiting time for first process is the arrival time of the process
+	wt[0] = processes[0];
+
+	// calculating waiting time
+	for (int i = 1; i < n; i++)
+		wt[i] = bt[i - 1] + wt[i - 1];
+
+	// calculating turnaround time by adding: bt[i] + wt[i]
+	for (int i = 0; i < n; i++) {
+		tat[i] = bt[i] + wt[i] - processes[i];
+
+		// Calculate total waiting time and total turn around time
+		total_wt = total_wt + wt[i];
+		total_tat = total_tat + tat[i];
+	}
+	float t = (float) total_tat / (float) n;
+	printf("FCFS: mean turnaround = %.2f\n", t);
+}
+
+
+void LCFSfindTurnAroundTime(int processes[], int n, int bt[], int preemptive) {
+	float t = 0;
+	if (preemptive == 1) printf("LCFS (P): mean turnaround = %.2f\n", t);
+	else if (preemptive == 0) printf("LCFS (NP): mean turnaround = %.2f\n", t);
+}
+
+
+int main(int argc, char *argv[]) {
+	int fd1;  // Input file descriptor
+	int n_procs;  // number of processes
+	int *processes;  // arrival time list
+	char * proc;
+	int *burst_time;  // Burst time of all processes
+	int i = 0;
+	char inputLineContent[BUFFER_SIZE];
+	if (argc != 2) {
+		printf("Usage: %s <input file>\n", argv[0]);
+		return 0;
+	}
+
+	// read config file
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 < 0) {
+		// printf("error open config file %s\n", argv[1]);
+		return 0;
+	}
+	// read the first line to indicates how many processes should be
+	readline(fd1, inputLineContent);
+	n_procs = inputLineContent[0] - '0';
+	// printf("Number of processes from input file: '%s' are: %d\n", argv[1], n_procs);
+
+	// create the array of numbers from file
+	processes = malloc(n_procs * sizeof(int *));
+	burst_time = malloc(n_procs * sizeof(int *));
+	for (i = 0; i < n_procs; i++) {
+		readline(fd1, inputLineContent);
+		// input string line is: "a,b"
+		// Returns first token
+		proc = strtok(inputLineContent, ",");
+		processes[i] = atoi(proc);
+		proc = strtok(NULL, ",");
+		burst_time[i] = atoi(proc);
+		// printf("%d burst-time of %d\n", processes[i], burst_time[i]);
+	}
+
+	close(fd1);  // config file is not needed anymore, closing it
+
+	// TODO: need to sort the processes list by arrival time
+
+	// FCFS: mean turnaround = ?
+	FCFSfindTurnAroundTime(processes, n_procs, burst_time);
+
+	// LCFS (NP): mean turnaround = ?
+	LCFSfindTurnAroundTime(processes, n_procs, burst_time, 0);
+
+	// LCFS (P): mean turnaround = ?
+	LCFSfindTurnAroundTime(processes, n_procs, burst_time, 1);
+
+//	// RR: mean turnaround = ?
+//	RRfindTurnAroundTime(processes, n_procs, burst_time);
+
+//	// SJF: mean turnaround = ?
+//	SJFfindTurnAroundTime(processes, n_procs, burst_time);
+
+	free(processes);
+	free(burst_time);
+	return 0;
+}
+
